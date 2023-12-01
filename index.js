c9b6924c6cf954b668f952eef83e1a87b1c04e43
Jasper Wong <wlcjasper@gmail.com>
2023-12-02 01:13:53 +0800

    handle cases where the image property is "N/A"

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ root.innerHTML = `
 <div class="dropdown">
 <div class="dropdown-menu">
   <div class="dropdown-content results">
-  </div>`
-  
-  const fetchData = async searchTerm => {
+  </div>`;
+
+const fetchData = async searchTerm => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: 'f354532a',
@@ -22,25 +22,25 @@ root.innerHTML = `
   return response.data.Search;
 };
 
-
-
 const userInput = document.querySelector("input");
-const dropdown= document.querySelector(".dropdown");
+const dropdown = document.querySelector(".dropdown");
 const resultsWrapper = document.querySelector(".results");
 
 const onInput = async () => {
+  resultsWrapper.innerHTML = "";
+  // makes more sense to do this before calling wait;
   const movies = await fetchData(userInput.value);
   // same as event.target.value;
   console.log(movies);
   dropdown.classList.add('is-active');
   for (let movie of movies) {
     const optionAnchor = document.createElement('a');
-    
+    const imgSrc = movie.Poster === "N/A" ? '' : movie.Poster;
+
     optionAnchor.classList.add('dropdown-item')
-    optionAnchor.innerHTML = `
-    <img src="${movie.Poster}" alt="image of one movie poster">
-    ${movie.Title}    
-    `
+    
+    optionAnchor.innerHTML = `<img src="${imgSrc}" />${movie.Title}`      
+
     resultsWrapper.appendChild(optionAnchor);
   }
 };
