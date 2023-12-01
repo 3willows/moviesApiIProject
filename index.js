21225f0a8a3f5b05f356c2f87bb7e04782a7c6dc
Jasper Wong <wlcjasper@gmail.com>
2023-12-01 22:50:58 +0800

    separate fetchInfo function

--- index.js
+++ index.js
@@ -2,20 +2,23 @@ const userInput = document.querySelector("#movieName");
 
 let timeoutId;
 
+const fetchInfo = (searchTerm) => {
+  axios.get(`https://www.omdbapi.com/?t=${searchTerm}&apikey=f354532a`)
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error("Error fetching data:", error);
+  }); 
+}
+
 const onInput = () => {
   const searchTerm = userInput.value;
   if (timeoutId) {
     clearTimeout(timeoutId);
   }
   timeoutId = setTimeout(() => { console.log(`${searchTerm}`);
-  axios.get(`https://www.omdbapi.com/?t=${searchTerm}&apikey=f354532a`)
-    .then(response => {
-      console.log(response.data);
-    })
-    .catch(error => {
-      console.error("Error fetching data:", error);
-    }); 
-}, 2000)
+  fetchInfo(searchTerm);}, 2000)
 };
 
 userInput.addEventListener("keypress", onInput);
