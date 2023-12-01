16da502a81e5bcdd40828014bcb1a67a9e6e044d
Jasper Wong <wlcjasper@gmail.com>
2023-12-02 00:54:33 +0800

    move dropdown menu to js

--- index.js
+++ index.js
@@ -1,31 +1,47 @@
-const fetchData = async searchTerm => {
+const root = document.querySelector(".autocomplete");
+root.innerHTML = ` 
+<label> <b>Search for a Movie</b></label>
+<input class = "input" />
+<div class="dropdown">
+<div class="dropdown-menu">
+  <div class="dropdown-content results">
+  </div>`
+  
+  const fetchData = async searchTerm => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: 'f354532a',
       s: searchTerm
     }
   })
-  .catch(() =>{
-  })
-  if (response.data.Error){
+    .catch(() => {
+    })
+  if (response.data.Error) {
     return [];
   }
   return response.data.Search;
 };
 
+
+
 const userInput = document.querySelector("input");
+const dropdown= document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
 
 const onInput = async () => {
   const movies = await fetchData(userInput.value);
   // same as event.target.value;
   console.log(movies);
-  for (let movie of movies){
-    const div = document.createElement('div');
-    div.innerHTML = `
-    <p>${movie.Title}</p>
+  dropdown.classList.add('is-active');
+  for (let movie of movies) {
+    const optionAnchor = document.createElement('a');
+    
+    optionAnchor.classList.add('dropdown-item')
+    optionAnchor.innerHTML = `
     <img src="${movie.Poster}" alt="image of one movie poster">
+    ${movie.Title}    
     `
-    document.querySelector("#target").append(div);
+    resultsWrapper.appendChild(optionAnchor);
   }
 };
 
