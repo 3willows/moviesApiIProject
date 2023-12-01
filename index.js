3ba150b8863c69535ccc62b0a853c6378ff92deb
Jasper Wong <wlcjasper@gmail.com>
2023-12-01 23:57:06 +0800

    add DOM manipulation

--- index.js
+++ index.js
@@ -5,13 +5,27 @@ const fetchData = async searchTerm => {
       s: searchTerm
     }
   })
-  console.log(response.data);
+  return response.data.Search;
 };
 
 const userInput = document.querySelector("input");
 
-const onInput = event => {
-  fetchData(event.target.value);
+const onInput = async () => {
+  const movies = await fetchData(userInput.value);
+  // same as event.target.value;
+  console.log(movies);
+  for (let movie of movies){
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <li>
+    <p>${movie.Title}</p>
+    <img src="${movie.Poster}" alt="image of one movie poster">
+    </li>
+    `
+    document.body.append(div);
+  }
 };
 
-userInput.addEventListener('input', debounce(onInput));
+const delayedInput = debounce(onInput);
+
+userInput.addEventListener('input', delayedInput);
