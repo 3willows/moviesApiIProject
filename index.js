18ff23dbc2249251ea8166e38c9377032e2133d6
Jasper Wong <wlcjasper@gmail.com>
2023-12-01 22:25:58 +0800

    add rough debouncing function

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Hi there!');
+const userInput = document.querySelector("#movieName");
+
+let timeoutId;
+
+userInput.addEventListener("keypress", () => {
+
+  const searchTerm = userInput.value;
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+  timeoutId = setTimeout(() => { console.log(`${searchTerm}`) }, 2000)
+  // axios.get(`https://www.omdbapi.com/?t=${userInput.value}&apikey=f354532a`)
+  //   .then(response => {
+  //     console.log(response.data);
+  //   })
+  //   .catch(error => {
+  //     console.error("Error fetching data:", error);
+  //   });
+});
+
